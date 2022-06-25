d0a5264a3fea4c4e99361149346addb3ff603dd3
leonidchashnikov <rayanral@gmail.com>
2022-06-25 20:12:25 +0100

    humble beginnings

---
+++ clox/debug.h
@@ -0,0 +1,13 @@
+//
+// Created by Leonid Chashnikov on 25/06/2022.
+//
+
+#ifndef CLOX_DEBUG_H
+#define CLOX_DEBUG_H
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif //CLOX_DEBUG_H
--- clox/main.c
+++ clox/main.c
@@ -1,11 +1,19 @@
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
-    writeChunk(&chunk, OP_RETURN);
+
+    int constantIndex = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constantIndex, 1);
+
+    writeChunk(&chunk, OP_RETURN, 1);
+
+    disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
 }
