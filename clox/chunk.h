2afb9e7f597ae7df55565f00f40e424fd4539351
leonidchashnikov <rayanral@gmail.com>
2022-06-30 20:04:54 +0100

    moar data types

--- clox/chunk.h
+++ clox/chunk.h
@@ -6,11 +6,18 @@
 
 typedef enum {
     OP_CONSTANT,
+    OP_NOT,
     OP_NEGATE,
     OP_ADD,
     OP_SUBTRACT,
     OP_MULTIPLY,
     OP_DIVIDE,
+    OP_NIL,
+    OP_TRUE,
+    OP_FALSE,
+    OP_EQUAL,
+    OP_GREATER,
+    OP_LESS,
     OP_RETURN,
 } OpCode;
 
--- clox/value.c
+++ clox/value.c
@@ -3,6 +3,15 @@
 #include "memory.h"
 #include "value.h"
 
+bool valuesEqual(Value a, Value b) {
+    if (a.type != b.type) return false;
+    switch (a.type) {
+        case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
+        case VAL_NIL:    return true;
+        case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+        default:         return false; // Unreachable.
+    }
+}
 
 void initValueArray(ValueArray* array) {
     array->values = NULL;
@@ -27,5 +36,11 @@ void freeValueArray(ValueArray* array) {
 }
 
 void printValue(Value value) {
-    printf("%g", value);
+    switch (value.type) {
+        case VAL_BOOL:
+            printf(AS_BOOL(value) ? "true" : "false");
+            break;
+        case VAL_NIL: printf("nil"); break;
+        case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+    }
 }
